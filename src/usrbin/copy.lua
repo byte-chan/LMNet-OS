3045c311a6d15416787729d1300ab740e4706576
Multi <fgflorianguenter@googlemail.com>
2014-04-24 14:23:32 +0200

    copy and move replace ~ with home path :D

---
+++ src/usrbin/copy.lua
@@ -0,0 +1,31 @@
+local tArgs = { ... }
+if #tArgs < 2 then
+	print( "Usage: cp <source> <destination>" )
+	return
+end
+
+local source = tArgs[1]
+if source:sub(1, 1) == "~" then
+	source = "/"..(currentUser == "root" and systemDirs.root or fs.combine(systemDirs.users, currentUser))..source:sub(2)
+end
+local sSource = shell.resolve( source )
+local dest = tArgs[2]
+if dest:sub(1, 1) == "~" then
+	dest = "/"..(currentUser == "root" and systemDirs.root or fs.combine(systemDirs.users, currentUser))..dest:sub(2)
+end
+local sDest = shell.resolve( dest )
+local tFiles = fs.find( sSource )
+if #tFiles > 0 then
+    for n,sFile in ipairs( tFiles ) do
+        if fs.isDir( sDest ) then
+            fs.copy( sFile, fs.combine( sDest, fs.getName(sFile) ) )
+        elseif #tFiles == 1 then
+            fs.copy( sFile, sDest )
+        else
+            printError( "Cannot overwrite file multiple times" )
+            return
+        end
+    end
+else
+    printError( "No matching files" )
+end
