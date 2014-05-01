edd1d3896a5f04e2a1e304afacf343cfeaf4cadb
Multi <fgflorianguenter@googlemail.com>
2014-05-01 18:32:41 +0200

    rdnt-srv: something. Version 1.2.

--- src/usrbin/rdnt-srv.lua
+++ src/usrbin/rdnt-srv.lua
@@ -40,7 +40,7 @@ local code = [[
 if not fs.exists("site") then
 	term.clear()
 	term.setCursorPos(1, 1)
-	print("No main site found. Exiting.")
+	print("No main site found. Create the file 'site'.")
 	return
 end
 
@@ -68,7 +68,7 @@ function printLog(text)
 	print("["..string.rep(" ", 5-time:len())..time.."] "..text)
 end
 
-printLog("rdnt-srv 1.1 started")
+printLog("rdnt-srv 1.2: "..url)
 
 local file = fs.open("/site", "r")
 local site = file.readAll()
@@ -80,6 +80,30 @@ while true do
 	if event == "rednet_message" and type(e[3]) == "string" then
 		local sender = e[2]
 		local msg = e[3]
+		local header = "local _DATA = {}\n"
+		local tmp = {string.gsub(msg, "[^?]+", "")}
+		if tmp[2] > 1 then
+			local matches = {}
+			for match in string.gmatch(msg, "[^?]+")} do
+				table.insert(matches, match)
+			end
+			local rawData = matches[2]
+			local parts = {}
+			for match in string.gmatch(rawData, "[^&]+") do
+				table.insert(parts, match)
+			end
+			local data = {}
+			for _, v in pairs(parts) do
+				local subparts = {}
+				for match in string.gmatch(v, "[^=]+") do
+					table.insert(subparts, match)
+				end
+				local key = subparts[1]
+				local value = subparts[2]
+				data[key] = value
+			end
+			header = "local _DATA = "..textutils.serialize(data).."\n"
+		end
 		if msg:sub(1, url:len()) == url and (msg:sub(url:len()+1, url:len()+1) == "" or msg:sub(url:len()+1, url:len()+1) == "/") then
 			local f = {string.gsub(msg, "[^/]+", "")}
 			if f[2] > 1 then
@@ -92,12 +116,12 @@ while true do
 				printLog("ID "..sender.." wants "..str)
 				if fs.exists("/subsite"..str) then
 					local file = fs.open("/subsite"..str, "r")
-					rednet.send(sender, file.readAll())
+					rednet.send(sender, header..file.readAll())
 					file.close()
 				else
 					if fs.exists("/404") then
 						local file = fs.open("/404", "r")
-						rednet.send(sender, file.readAll())
+						rednet.send(sender, header..file.readAll())
 						file.close()
 					else
 						rednet.send(sender, "print(\"404 Not Found\")\nprint(\"This file does not exist on this site.\")")
@@ -105,7 +129,7 @@ while true do
 					printLog("Reply to ID "..sender..": 404")
 				end
 			else
-				rednet.send(sender, site)
+				rednet.send(sender, header..site)
 				printLog("ID "..sender.." wants main site")
 			end
 			printLog("Request by ID "..sender..": success.")
