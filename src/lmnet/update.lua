a66fd223ddbdd7eb8542088e7575218c9db04afa
Multi <fgflorianguenter@googlemail.com>
2014-05-31 10:41:49 +0200

    Ask user to replace old startup

--- src/lmnet/update.lua
+++ src/lmnet/update.lua
@@ -23,6 +23,74 @@ local function get(user, repo, bran, path, save)
 	return text
 end
 
+local function yesno(text, title, start)
+	local function clear()
+		term.clear()
+		term.setCursorPos(1, 1)
+	end
+	
+	local function drawButton(buttonText, x, y, x2, y2, enabled)
+		if enabled then
+			term.setBackgroundColor(colors.white)
+		else
+			if term.isColor() then
+				term.setBackgroundColor(colors.gray)
+			end
+		end
+		term.setCursorPos(x, y)
+		for _y = y, y2 do
+			for _x = x, x2 do
+				term.setCursorPos(_x, _y)
+				write(" ")
+			end
+		end
+		term.setCursorPos(x+math.floor((x2-x)/2)-math.floor(buttonText:len()/2), y+math.floor((y2-y+1)/2))
+		term.setTextColor(colors.black)
+		write(buttonText)
+		term.setTextColor(colors.white)
+		term.setBackgroundColor(colors.black)
+	end
+	
+	local function cprint(text)
+		local x, y = term.getCursorPos()
+		local w, h = term.getSize()
+		term.setCursorPos(math.floor(w/2)-math.floor(text:len()/2), y)
+		print(text)
+	end
+	
+	local selected = true
+	
+	local function redraw()
+		clear()
+		cprint(title)
+		term.setCursorPos(1, 3)
+		print(text)
+		local w, h = term.getSize()
+		drawButton("Yes", 2, h-1, math.floor(w/2)-1, h-1, selected)
+		drawButton("No", math.floor(w/2)+1, h-1, w-1, h-1, not selected)
+	end
+	
+	if start ~= nil and type(start) == "boolean" then
+		selected = start
+	end
+	while true do
+		redraw()
+		local eventData = {os.pullEventRaw()}
+		if eventData[1] == "terminate" then
+			clear()
+			return
+		elseif eventData[1] == "key" then
+			if eventData[2] == keys.up or eventData[2] == keys.down or eventData[2] == keys.left or eventData[2] == keys.right then
+				selected = not selected
+			elseif eventData[2] == keys.enter then
+				clear()
+				return selected
+			end
+		end
+		sleep(0)
+	end
+end
+
 local function getFile(file, target)
 	return get("MultHub", "LMNet-OS", "master", file, target)
 end
@@ -34,7 +102,7 @@ clear()
 print("LMNet OS - update or install")
 print("Getting files...")
 local files = {
-	["src/startup.lua"] = "startup",
+	["src/startup.lua"] = "lmnet",
 	["src/lmnet/init.lua"] = ".lmnet/init",
 	["src/lmnet/login.lua"] = ".lmnet/login",
 	["src/lmnet/update.lua"] = ".lmnet/update",
@@ -109,6 +177,21 @@ local dirs = {
 for _, v in pairs(dirs) do
 	if not fs.exists(v) then
 		fs.makeDir(v)
+	end
+end
+if fs.exists("startup") and not fs.isDir("startup") then
+	local oldSu = fs.open("startup", "r")
+	local oSu = oldSu.readAll()
+	oldSu.close()
+	local newSu = fs.open("lmnet", "r")
+	local nSu = newSu.readAll()
+	newSu.close()
+	if oSu ~= nSu then
+		local overwrite = yesno("Replace old startup?\n\n(\"No\" to run LMNet OS with \"lmnet\")", "Old startup detected.", false)
+		if overwrite then
+			fs.delete("startup")
+			fs.copy("lmnet", "startup")
+		end
 	end
 end
 print("Press any key to continue")
