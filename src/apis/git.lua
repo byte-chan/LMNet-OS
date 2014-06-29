34fe6d72175747570d3813965e5474340a4565e6
timia2109 <tim.ittermann@gmail.com>
2014-06-29 14:37:51 +0200

    Prepare for using the GitHub API. Comming soon!

--- src/apis/git.lua
+++ src/apis/git.lua
@@ -1,3 +1,27 @@
+local function request(pURL)
+	if type(pURL) == 'table' then
+		local x = ''
+		for i=1,#pURL do
+			x = x..pURL[i]..'/'
+		end
+		pURL = x
+	end
+	local URL = textutils.urlEncode('https://api.github.com/'..pURL)
+	if URL:sub(URL:len(),URL:len()) == "/" then
+		URL = URL:sub(1,URL:len()-1)
+	end
+	local res = http.get(URL)
+	if res then
+		return json.decode(res.readAll())
+	else
+		return nil
+	end
+end
+
+function getUser(user,content)
+	return request({'users',user,content})
+end
+
 function get(user, repo, bran, path, save)
 	if not user or not repo or not bran or not path then
 		error("not enough arguments, expected 4 or 5", 2)
