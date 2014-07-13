730080928d9687b4ba7f00d07d551d09aec5dd72
Tim Ittermann <tim.ittermann@gmail.com>
2014-07-13 17:33:43 +0200

    .update is now in root :D

--- src/usrbin/updater.lua
+++ src/usrbin/updater.lua
@@ -1,4 +1,4 @@
 if not http then printError("HTTP not enabled") return end
 local remote = http.get("https://raw.github.com/MultHub/LMNet-OS/master/src/lmnet/update.lua")
 if not remote then return end
-local file = fs.open(".update", "w") file.write(remote.readAll()) file.close() remote.close() shell.run(".update")
+local file = fs.open("/.update", "w") file.write(remote.readAll()) file.close() remote.close() shell.run("/.update")
