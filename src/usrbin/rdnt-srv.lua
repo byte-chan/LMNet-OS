ee0738dc71658341fa8a3ff83a592961f58346c8
Multi <MultHub@users.noreply.github.com>
2014-07-17 21:11:09 +0200

    Update rdnt-srv installer to get from extra branch
    Because no one wants to update LMNet OS to get 1 file updated.

--- src/usrbin/rdnt-srv.lua
+++ src/usrbin/rdnt-srv.lua
@@ -33,122 +33,25 @@ while not urlOK do
 	end
 end
 
-local code = [[
--- rdnt-srv 1.1
--- Server software for rdnt
-
-if not fs.exists("site") then
-	term.clear()
-	term.setCursorPos(1, 1)
-	print("No main site found. Create the file 'site'.")
+local remote = http.get("https://raw.github.com/MultHub/LMNet-OS/extra/rdnt-server.lua")
+if remote then
+	local file = fs.open(".lmnet/rdnt-server.lua", "w")
+	file.write(remote.readAll())
+	file.close()
+	remote.close()
+end
+if not fs.exists(".lmnet/rdnt-server.lua") then
+	printError("Error installing rdnt-srv.")
+	printError("Please try again later.")
 	return
 end
 
-local oldPullEvent = os.pullEvent
-os.pullEvent = os.pullEventRaw
-
-for _, v in pairs(rs.getSides()) do
-	if peripheral.getType(v) == "modem" then
-		rednet.open(v)
+if fs.exists("startup") then
+	if fs.exists("startup.rdntbak") then
+		fs.delete("startup.rdntbak")
 	end
+	fs.move("startup", "startup.rdntbak")
 end
-
-local url = "]]..url..[["
-
-
-function clear()
-	term.clear()
-	term.setCursorPos(1, 1)
-end
-
-clear()
-
-function printLog(text)
-	local time = textutils.formatTime(os.time(), true)
-	print("["..string.rep(" ", 5-time:len())..time.."] "..text)
-end
-
-printLog("rdnt-srv 1.2: "..url)
-
-local file = fs.open("/site", "r")
-local site = file.readAll()
-file.close()
-
-while true do
-	local e = {os.pullEvent()}
-	local event = e[1]
-	if event == "rednet_message" and type(e[3]) == "string" then
-		local sender = e[2]
-		local msg = e[3]
-		local header = "local _DATA = {}\n"
-		local tmp = {string.gsub(msg, "[^?]+", "")}
-		if tmp[2] > 1 then
-			local matches = {}
-			for match in string.gmatch(msg, "[^?]+") do
-				table.insert(matches, match)
-			end
-			local rawData = matches[2]
-			local parts = {}
-			for match in string.gmatch(rawData, "[^&]+") do
-				table.insert(parts, match)
-			end
-			local data = {}
-			for _, v in pairs(parts) do
-				local subparts = {}
-				for match in string.gmatch(v, "[^=]+") do
-					table.insert(subparts, match)
-				end
-				local key = subparts[1]
-				local value = subparts[2]
-				data[key] = value
-			end
-			header = "local _DATA = "..textutils.serialize(data).."\n"
-		end
-		if msg:sub(1, url:len()) == url and (msg:sub(url:len()+1, url:len()+1) == "" or msg:sub(url:len()+1, url:len()+1) == "/") then
-			local f = {string.gsub(msg, "[^/]+", "")}
-			if f[2] > 1 then
-				local str = ""
-				local matches = {}
-				for match in string.gmatch(msg, "[^?]+") do
-					table.insert(matches, match)
-				end
-				for match in string.gmatch(matches[1], "[^/]+") do
-					if match ~= url then
-						str = str.."/"..match
-					end
-				end
-				printLog("ID "..sender.." wants "..str)
-				if fs.exists("/subsite"..str) then
-					local file = fs.open("/subsite"..str, "r")
-					rednet.send(sender, header..file.readAll())
-					file.close()
-				else
-					if fs.exists("/404") then
-						local file = fs.open("/404", "r")
-						rednet.send(sender, header..file.readAll())
-						file.close()
-					else
-						rednet.send(sender, "print(\"404 Not Found\")\nprint(\"This file does not exist on this site.\")")
-					end
-					printLog("Reply to ID "..sender..": 404")
-				end
-			else
-				rednet.send(sender, header..site)
-				printLog("ID "..sender.." wants main site")
-			end
-			printLog("Request by ID "..sender..": success.")
-		end
-	elseif event == "terminate" then
-		printLog("Exiting.")
-		sleep(0.1)
-		os.pullEvent = oldPullEvent
-		return
-	end
-end
-]]
-
-local file = fs.open("startup", "w")
-file.write(code)
-file.close()
+fs.copy(".lmnet/rdnt-server.lua", "startup")
 
 print("rdnt-srv installed. Create the file 'site' if it doesn't exist.")
