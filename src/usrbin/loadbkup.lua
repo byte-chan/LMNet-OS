be86876a35d4aa6ff67d4f0c2c9374c9092f5a52
Connor Delaney <connordelaneyy@gmail.com>
2014-07-25 23:19:53 +1000

    added disk checking and main stuffs

--- src/usrbin/loadbkup.lua
+++ src/usrbin/loadbkup.lua
@@ -1,1 +1,14 @@
--- LoadBKUP
+print("LoadBackup: Awaiting a blank floppy disk")
+while true do
+  local event, side = os.pullEvent()
+  if event == "disk" then
+    print("LoadBackup: Would you like to load: '"..os.getLabel(side).."' ? y/n")
+    print("NOTE: This will erase all existing files")
+    local input = read()
+    if input == "y" then
+      print("
+    else
+      print("LoadBackup: Operation Cancelled")
+    end
+  end
+end
