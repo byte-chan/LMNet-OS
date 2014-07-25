b04ca28e531b359bb4d27c941f48a4a075451cd8
Connor Delaney <connordelaneyy@gmail.com>
2014-07-25 23:23:18 +1000

    added deletion of files & applying backup

--- src/usrbin/loadbkup.lua
+++ src/usrbin/loadbkup.lua
@@ -6,9 +6,20 @@ while true do
     print("NOTE: This will erase all existing files")
     local input = read()
     if input == "y" then
-      print("
+      local files = fs.list("/")
+      for i = 1, #files do
+        fs.delete(files[i])
+        print("LoadBackup: Deleted "..files[i])
+      end
+        local files = fs.list("/disk/")
+        for i = 1, #files do
+          fs.copy("/disk/"..files[i], "/"..files[i])
+          print("LoadBackup: Restored "..files[i])
+        end
+      return
     else
       print("LoadBackup: Operation Cancelled")
+      return
     end
   end
 end
