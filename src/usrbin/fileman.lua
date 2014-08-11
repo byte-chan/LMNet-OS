4803e57cd5655f773d4652858d5de7a9ce6b00e5
Tim Ittermann <timia2109@users.noreply.github.com>
2014-08-11 23:09:04 +0200

    Both versions now have the same menu!

--- src/usrbin/fileman.lua
+++ src/usrbin/fileman.lua
@@ -2,14 +2,111 @@ local tArgs = { ... }
 local fileMans = {}
 local edit = 'edit' --or luaide ;D
 
+local popup,workPath,saveFile
+local xLen,yLen = term.getSize()
+
+
+if not ui then
+	local whereCanBe = {'ui','.lmnet/apis/ui','apis/ui'}
+	local loaded = false
+	for i=1,#whereCanBe do
+		if fs.exists(whereCanBe[i]) then
+			os.loadAPI(whereCanBe[i])
+			loaded = true
+		end
+	end
+	if not loaded then
+		printError("LMNet UI API is missing...")
+		print("Start downloading as /ui...")
+		local h = http.get('https://raw.github.com/MultHub/LMNet-OS/master/src/apis/ui.lua')
+		if h then
+			local f = fs.open("ui",'w')
+			f.write(h.readAll())
+			f.close()
+			h.close()
+			shell.run(shell.getRunningProgram())
+			error(nil,0)
+		else
+			error("Can't get UI API! Is github on http whitelist?",0)
+		end
+	end
+end
+
+local function getFileMenu(isDir)
+	local waysActs = {
+		['Delete'] = function(pFile) if ui.yesno('Are you shure?','Delete '..pFile) then fs.delete(fs.combine(shell.dir(),pFile)) end end,
+		['Rename'] = function(pFile) fs.move(pFile,popup("New File Name: ",'input')) end,
+		['Copy'] = function(pFile) saveFile = '0'..fs.combine(shell.dir(),pFile) end,
+		['Cut'] = function(pFile) saveFile = '1'..fs.combine(shell.dir(),pFile) end,
+		['Back'] = function() end,
+	}
+
+	local ways = {}
+
+	if isDir then
+		waysActs['Open'] = function(pFile) shell.setDir(pFile) if term.isColor() then listFolder(pFile,1) end end
+		ways[1] = 'Open'
+	else
+		waysActs['Run'] = function(pFile) shell.run(pFile) end
+		ways[1] = 'Run'
+		waysActs['Run with Args'] = function(pFile) shell.run(pFile,popup('Args: ','input')) end
+		waysActs['Edit'] = function(pFile) shell.run(edit,pFile) end
+		waysActs['Upload to Pastebin'] = function(pFile) shell.run('pastebin','put',pFile) end
+	end
+
+	if saveFile then
+		waysActs['Paste'] = function() if saveFile:sub(1,1) == '0' then fs.copy(saveFile:sub(2,saveFile:len()),shell.dir()) end end
+	end
+
+	for i,v in pairs(waysActs) do
+		if i ~= 'Open' or i ~= 'Run' or i ~= 'Back' then
+			table.insert(ways,i)
+		end
+	end
+	ways[#ways+1] = 'Back'
+
+	return ways,waysActs
+end
+
+function popup(pTxt,pMode)
+	term.clear()
+	local bCol,tCol,tfCol
+	if term.isColor() then
+		bCol = colors.lightGray
+		rtCol = colors.white
+		tCol = colors.black
+		tfCol = colors.white
+	else
+		bCol = colors.white
+		rtCol = colors.black
+		tCol = colors.white
+		tfCol = colors.black
+	end
+	local StartY = (yLen/2)-(3)
+	for i=0,4 do
+		paintutils.drawLine(1,StartY+i,xLen,StartY+i,bCol)
+	end
+	term.setCursorPos(1,StartY+1)
+	term.setTextColor(rtCol)
+	ui.cprint(pTxt)
+
+	if pMode == 'input' then
+		paintutils.drawLine(3,StartY+3,xLen-2,StartY+3,tfCol)
+		term.setTextColor(tCol)
+		term.setCursorPos(3,StartY+3)
+		return read()
+	elseif pMode == 'msg' then
+		sleep(3)
+	end
+end
+
 fileMans[true] = function()
-	local xLen,yLen = term.getSize()
 
 	local color = {}
 	color.dir = colors.yellow
 	color.file = colors.white
 
-	local workPath,saveFile,workCount,isDir,changePath,rmenu,popup,listFolder
+	local workCount,isDir,changePath,rmenu,listFolder
 
 	function isDir(pWat)
 		return fs.isDir(workPath..'/'..pWat)
@@ -33,26 +130,16 @@ fileMans[true] = function()
 	end
 
 	function rmenu(pFile)
-		local waysActs = {
-			['Open with Args'] = function() shell.run(pFile,popup('Args: ','input')) end,
-			['Edit'] = function() shell.run(edit,pFile) end,
-			['Upload to Pastebin'] = function() shell.run('pastebin','put',pFile) end,
-			['Delete'] = function() if ui.yesno('Are you shure?','Delete '..pFile) then fs.delete(fs.combine(shell.dir(),pFile)) end end,
-			['Rename'] = function() fs.move(pFile,popup("New File Name: ",'input')) end,
-			['Copy'] = function() saveFile = '0'..fs.combine(shell.dir(),pFile) end,
-			['Cut'] = function() saveFile = '1'..fs.combine(shell.dir(),pFile) end,
-		}
+		local ways,waysActs = getFileMenu(fs.isDir(pFile))
+		term.clear()
+		local lMessage = ''
 
-		local ways = {'Open with Args','Edit','Upload to Pastebin','Delete','Rename','Copy','Cut'}
-
-		if saveFile then
-			waysActs['Paste'] = function() if saveFile:sub(1,1) == '0' then fs.copy(saveFile:sub(2,saveFile:len()),shell.dir()) end end
-			table.insert(ways,'Paste')
+		for i=1,#ways do
+			if ways[i]:len() > lMessage:len() then
+				lMessage = ways[i]
+			end
 		end
 
-		term.clear()
-
-		local lMessage = 'Upload to Pastebin'
 		lMessage = lMessage:len()
 
 		local StartY = (yLen/2)-(#ways/2)
@@ -81,32 +168,13 @@ fileMans[true] = function()
 		while menu do
 			local evI = { os.pullEvent() }
 			if evI[1] == 'mouse_click' then
-				if mAct[evI[4]] then
-					waysActs[mAct[evI[4]]]()
+				if evI[3] >= StartX-1 and evI[3] <= EndX+1 then
+					if mAct[evI[4]] then
+						waysActs[mAct[evI[4]]]()
+					end
 				end
 				menu = false
 			end
-		end
-	end
-
-	function popup(pTxt,pMode)
-		term.clear()
-
-		local StartY = (yLen/2)-(3)
-		for i=0,4 do
-			paintutils.drawLine(1,StartY+i,xLen,StartY+i,colors.lightGray)
-		end
-		term.setCursorPos(1,StartY+1)
-		term.setTextColor(colors.white)
-		ui.cprint(pTxt)
-
-		if pMode == 'input' then
-			paintutils.drawLine(3,StartY+3,xLen-2,StartY+3,colors.white)
-			term.setTextColor(colors.black)
-			term.setCursorPos(3,StartY+3)
-			return read()
-		elseif pMode == 'msg' then
-			sleep(3)
 		end
 	end
 
@@ -254,17 +322,7 @@ fileMans[true] = function()
 end
 
 fileMans[false] = function()
-	if not ui then
-		local success = os.loadAPI(".lmnet/apis/ui")
-		if not success then
-			local success2 = os.loadAPI("ui")
-			if not success2 then
-				printError("UI API not found")
-				return
-			end
-		end
-	end
-
+	
 	local function itemList()
 		local items = fs.list(shell.dir())
 		table.sort(items)
@@ -278,29 +336,18 @@ fileMans[false] = function()
 		local selected = ui.menu(itemList(), "/"..shell.dir())
 		if selected then
 			if fs.isDir(shell.resolve(selected)) then
-				local selectedAction = ui.menu({"Go to", "Delete", "Move", "Copy", "New"}, "Actions for /"..shell.resolve(selected))
+				local ways,waysActs = getFileMenu(true)
+				local selectedAction = ui.menu(ways, "Actions for /"..shell.resolve(selected))
 				if selectedAction then
-					if selectedAction == "Go to" then
-						shell.setDir(shell.resolve(selected))
-						return true
-					elseif selectedAction == "Delete" then
-						fs.delete(shell.resolve(selected))
-						return true
-					end
+					waysActs[selectedAction](shell.resolve(selected))
+					return true
 				end
 			else
-				local selectedAction = ui.menu({"Run", "Edit", "Delete", "Move", "Copy", "New"}, "Actions for /"..shell.resolve(selected))
+				local ways,waysActs = getFileMenu(false)
+				local selectedAction = ui.menu(ways, "Actions for /"..shell.resolve(selected))
 				if selectedAction then
-					if selectedAction == "Run" then
-						shell.run(selected)
-						return true
-					elseif selectedAction == "Edit" then
-						shell.run(edit.." \""..selected.."\"")
-						return true
-					elseif selectedAction == "Delete" then
-						fs.delete(shell.resolve(selected))
-						return true
-					end
+					waysActs[selectedAction](shell.resolve(selected))
+					return true
 				end
 			end
 		end
