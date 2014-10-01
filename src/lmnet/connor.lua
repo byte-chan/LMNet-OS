279654b37a1af84b0113934934948f6884e453e1
Connor Delaney <connordelaneyy@gmail.com>
2014-10-01 22:29:07 +1000

    Update connor.lua

--- src/lmnet/connor.lua
+++ src/lmnet/connor.lua
@@ -8,7 +8,7 @@ os.squid = function()
   local chance = math.random(1, 3)
   if chance == 3 then
     term.setTextColour(colours.blue)
-    print("The Squid goes Splash 
+    print("The Squid goes Splash")
     term.setTextColour(colours.white)
   else
     term.setTextColour(colours.grey)
