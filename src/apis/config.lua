c2c86f71d9b154c5e8eb93edd5a2a74337b1de47
Multi <MultHub@users.noreply.github.com>
2014-12-21 11:26:40 +0100

    Should be safe now...
    a crap ton of GLaDOS cores actually fried the system...
    
    so this change should fix

--- src/apis/config.lua
+++ src/apis/config.lua
@@ -1,68 +1,27 @@
 function read(cfgfile, cfg)
-	local path
-	if not cfgfile then
-		path = "/.lmnet/sys.conf"
-	else
-		path = cfgfile
-	end
-	local file = fs.open(path, "r")
-	if not file then
-		return nil
-	end
-	local lines = {}
-	local line = ""
-	while line ~= nil do
-		line = file.readLine()
-		if line then
-			table.insert(lines, line)
+	local rtn
+	local ok, err = pcall(function()
+		local path
+		if not cfgfile then
+			path = "/.lmnet/sys.conf"
+		else
+			path = cfgfile
 		end
-	end
-	file.close()
-	local config = {}
-	for _, v in pairs(lines) do
-		local tmp
-		local tmp2 = ""
-		for match in string.gmatch(v, "[^\=]+") do
-			if tmp then
-				tmp2 = tmp2..match
-			else
-				tmp = match
+		local file = fs.open(path, "r")
+		if not file then
+			return nil
+		end
+		local lines = {}
+		local line = ""
+		while line ~= nil do
+			line = file.readLine()
+			if line then
+				table.insert(lines, line)
 			end
 		end
-		config[tmp] = textutils.unserialize(tostring(tmp2))
-	end
-	for i, v in pairs(config) do
-		if i == cfg then
-			return v
-		end
-	end
-	return nil
-end
-function write(cfgfile, cfg, value)
-	local path
-	if not cfgfile then
-		path = "/.lmnet/sys.conf"
-	else
-		path = cfgfile
-	end
-	if fs.exists(path) and fs.isDir(path) then
-		error(path.." is a directory", 0)
-	end
-	local readfile = fs.open(path, "r")
-	local readlines = {}
-	if readfile then
-		local readline = ""
-		while readline ~= nil do
-			readline = readfile.readLine()
-			if readline then
-				table.insert(readlines, readline)
-			end
-		end
-		readfile.close()
-	end
-	local config = {}
-	if fs.exists(path) and not fs.isDir(path) then
-		for _, v in pairs(readlines) do
+		file.close()
+		local config = {}
+		for _, v in pairs(lines) do
 			local tmp
 			local tmp2 = ""
 			for match in string.gmatch(v, "[^\=]+") do
@@ -74,47 +33,106 @@ function write(cfgfile, cfg, value)
 			end
 			config[tmp] = textutils.unserialize(tostring(tmp2))
 		end
-	end
-	local writefile = fs.open(path, "w")
-	config[cfg] = value
-	table.sort(config)
-	for i, v in pairs(config) do
-		writefile.writeLine(i.."="..textutils.serialize(v))
-	end
-	writefile.close()
-end
-function list(cfgfile)
-	local path
-	if not cfgfile then
-		path = "/.lmnet/sys.conf"
-	else
-		path = cfgfile
-	end
-	local file = fs.open(path, "r")
-	if not file then
-		return nil
-	end
-	local lines = {}
-	local line = ""
-	while line ~= nil do
-		line = file.readLine()
-		if line then
-			table.insert(lines, line)
-		end
-	end
-	file.close()
-	local config = {}
-	for _, v in pairs(lines) do
-		local tmp
-		local tmp2 = ""
-		for match in string.gmatch(v, "[^\=]+") do
-			if tmp then
-				tmp2 = tmp2..match
-			else
-				tmp = match
+		for i, v in pairs(config) do
+			if i == cfg then
+				rtn = v
 			end
 		end
-		config[tmp] = textutils.unserialize(tostring(tmp2))
+	end)
+	if not ok then
+		return false, err
 	end
-	return config
+	return rtn
+end
+function write(cfgfile, cfg, value)
+	local rtn = false
+	local ok, err = pcall(function()
+		local path
+		if not cfgfile then
+			path = "/.lmnet/sys.conf"
+		else
+			path = cfgfile
+		end
+		if fs.exists(path) and fs.isDir(path) then
+			error(path.." is a directory", 0)
+		end
+		local readfile = fs.open(path, "r")
+		local readlines = {}
+		if readfile then
+			local readline = ""
+			while readline ~= nil do
+				readline = readfile.readLine()
+				if readline then
+					table.insert(readlines, readline)
+				end
+			end
+			readfile.close()
+		end
+		local config = {}
+		if fs.exists(path) and not fs.isDir(path) then
+			for _, v in pairs(readlines) do
+				local tmp
+				local tmp2 = ""
+				for match in string.gmatch(v, "[^\=]+") do
+					if tmp then
+						tmp2 = tmp2..match
+					else
+						tmp = match
+					end
+				end
+				config[tmp] = textutils.unserialize(tostring(tmp2))
+			end
+		end
+		local writefile = fs.open(path, "w")
+		config[cfg] = value
+		table.sort(config)
+		for i, v in pairs(config) do
+			writefile.writeLine(i.."="..textutils.serialize(v))
+		end
+		writefile.close()
+		rtn = true
+	end
+	return rtn
+end
+function list(cfgfile)
+	local rtn
+	local ok, err = pcall(function()
+		local path
+		if not cfgfile then
+			path = "/.lmnet/sys.conf"
+		else
+			path = cfgfile
+		end
+		local file = fs.open(path, "r")
+		if not file then
+			return nil
+		end
+		local lines = {}
+		local line = ""
+		while line ~= nil do
+			line = file.readLine()
+			if line then
+				table.insert(lines, line)
+			end
+		end
+		file.close()
+		local config = {}
+		for _, v in pairs(lines) do
+			local tmp
+			local tmp2 = ""
+			for match in string.gmatch(v, "[^\=]+") do
+				if tmp then
+					tmp2 = tmp2..match
+				else
+					tmp = match
+				end
+			end
+			config[tmp] = textutils.unserialize(tostring(tmp2))
+		end
+		rtn = config
+	end)
+	if not ok then
+		return false, err
+	end
+	return rtn
 end
